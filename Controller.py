eed3c53139703ebeb51b7ce1d2b1073c19b5f5ff
amin <aminbalouchi@outlook.com>
2020-04-01 17:18:28 +0430

    test

---
+++ Controller.py
@@ -0,0 +1,15 @@
+import web
+
+urls =(
+    '/','home'
+)
+render = web.template.render("Views/Templates",base="MainLayout")
+app = web.application(urls, globals())
+#Classes/Routes
+
+class home :
+    def GET(self):
+        return render.home()
+
+if __name__=="__main__":
+    app.run()
